e7b6e20ca9991a89a1c397a66fb9e51e58a4cd90
thp91 <78363763+thp91@users.noreply.github.com>
2023-09-04 21:11:00 +0200

    test css

--- frontend/src/Pages/Dashboard/DashboardPage.js
+++ frontend/src/Pages/Dashboard/DashboardPage.js
@@ -15,30 +15,34 @@ import './style.scss'
 
 
 function Dashboard() {
-    
-    return <div className="">
-        <NavbarDashboard></NavbarDashboard>
-                <div classNamme="dashboard-global-container">
-                    <div className="dashboard-center">
-                                    <div className="dashboard-category">A découvrir</div>
-                                    <Discovercard title={"test"} ></Discovercard>
-                                    <div className="dashboard-category">Vos cours</div>
-                                    <SessionCard title={"Concerto  No.5 in A Majorr"} date={"23/01/10"} pourcentage={24}></SessionCard>
 
-                                    <div className="dashboard-category"> Dernier Cours Publier</div>
-                                    <div className="dashboard-category">Cours Terminé</div>    
-                            </div>
-                    <div className="right-container">
-                                    <div className="dashboard-category">Messagerie</div>
-                                    <div className="dashboard-category">Calendrier</div>
-                                </div>  
-                    <div className='left-container'>
-                                    <div className="dashboard-category">Dashboard</div>
-                            </div>  
-                </div>                            
+        return <div className="">
+                <NavbarDashboard></NavbarDashboard>
+                <div classNamme="dashboard-global-container">
+
+                        <div className='left-container'>
+                                <div className="dashboard-category">Dashboard</div>
+                        </div>
+
+                        <div className="dashboard-center">
+                                <div className="dashboard-category">A découvrir</div>
+                                <Discovercard title={"test"} ></Discovercard>
+                                <div className="dashboard-category">Vos cours</div>
+                                <SessionCard title={"Concerto  No.5 in A Majorr"} date={"23/01/10"} pourcentage={24}></SessionCard>
+
+                                <div className="dashboard-category"> Dernier Cours Publier</div>
+                                <div className="dashboard-category">Cours Terminé</div>
+                        </div>
+
+                        <div className="right-container">
+                                <div className="dashboard-category">Messagerie</div>
+                                <div className="dashboard-category">Calendrier</div>
+                        </div>
+                        
+                </div>
         </div>
-        
-    
+
+
 }
 
 export default Dashboard;
