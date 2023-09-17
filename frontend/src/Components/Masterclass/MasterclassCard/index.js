0ffb7a4a3c3c33b690f6c3545fe792e0b17b7ef1
Nathan Krumenacker <nathankrumenacker@MacBook-Pro-de-Nathan.local>
2023-09-17 18:40:33 +0200

    added filter and pagination

--- frontend/src/Components/Masterclass/MasterclassCard/index.js
+++ frontend/src/Components/Masterclass/MasterclassCard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from '../../../assets/image/index'
 import './style.scss'
 
-function MasterclassCard({image,title,duration, creator, comment, level}) {
+function MasterclassCard({image,title,instrument, creator, comment, level}) {
     return (
         <div className="masterclass-card">
             <img src={image} alt={"masterclassimage"}/>
@@ -16,7 +16,7 @@ function MasterclassCard({image,title,duration, creator, comment, level}) {
                     </div>
                     <div className="masterclass-duration">
                         <img src={Image.Circle}/>
-                        <p>{duration}</p>
+                        <p>{instrument}</p>
                     </div>
                 </div>
                 <p className="masterclass-description">{comment}</p>
--- frontend/src/Pages/MasterclassPage/index.js
+++ frontend/src/Pages/MasterclassPage/index.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
+import PwButton from '../../Components/button';
+import MasterclassCard from '../../Components/Masterclass/MasterclassCard';
+import './style.scss';
+import Footer from '../../Components/Footer';
 
-import PwButton from "../../Components/button";
-import MasterclassCard from "../../Components/Masterclass/MasterclassCard";
-
-import './style.scss'
-import Footer from "../../Components/Footer";
-
-function Masterclasspage() {
+function MasterclassPage() {
     const [error, setError] = useState(null);
-    const [masterclass, setMasterclasses] = useState([]); // Initialisé en tant qu'array vide
+    const [masterclass, setMasterclasses] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const masterclassesPerPage = 5;
+    const [activeFilter, setActiveFilter] = useState(null);
 
     const getMasterclass = async () => {
         try {
             const response = await axios.get('http://localhost:8080/masterclass');
-            setMasterclasses(response.data); // Utilisation de response.data pour accéder aux données
-            console.log('Response from server:', response.data); // Log de la réponse
+            setMasterclasses(response.data);
+            console.log('Response from server:', response.data);
         } catch (err) {
-            console.error('Error during fetching masterclasses:', err); // Log des erreurs
+            console.error('Error during fetching masterclasses:', err);
             setMasterclasses([]);
 
-            // Affichez une erreur détaillée si elle est fournie dans la réponse
             if (err.response && err.response.data && err.response.data.message) {
                 setError(err.response.data.message);
             } else {
@@ -28,43 +28,91 @@ function Masterclasspage() {
             }
         }
     };
+    const  getLessons = async () => {
+
+    }
 
     useEffect(() => {
         getMasterclass();
-    }, []); // [] en tant que dépendance pour une exécution unique
+    }, []);
+
+    const applyFilter = (filter) => {
+        // Apply the filter and set the active filter
+        // Implement your logic to filter the masterclasses based on the selected filter
+        // For simplicity, let's assume you have a field in masterclass called 'category'
+        setActiveFilter(filter);
+        // Modify this line based on your filter logic
+        const filteredMasterclasses = masterclass.filter((m) => m.category === filter);
+        setMasterclasses(filteredMasterclasses);
+        setCurrentPage(1); // Reset to the first page after applying the filter
+    };
+
+
+
 
     const renderMasterclasses = () => {
-        // Vérification de l'existence de masterclass avant le mappage
+        const indexOfLastMasterclass = currentPage * masterclassesPerPage;
+        const indexOfFirstMasterclass = indexOfLastMasterclass - masterclassesPerPage;
+        const currentMasterclasses = masterclass.slice(indexOfFirstMasterclass, indexOfLastMasterclass);
+
         if (!masterclass) {
             return null;
         }
-        return masterclass.map(n => <MasterclassCard masterclass={n}
-                                                     key={n.id}
-                                                     title={n.name}
-                                                     creator={n.created_by}
-                                                     level={n.level}
-                                                     duration={n.duration}
-                                                     image={n.image}
-                                                     comment={n.comment} />);
+
+        return currentMasterclasses.map((n) => (
+            <MasterclassCard
+                masterclass={n}
+                key={n.id}
+                title={n.name}
+                creator={n.created_by}
+                level={n.level}
+                duration={n.duration}
+                image={n.image}
+                comment={n.comment}
+                instrument={n.category}
+            />
+        ));
     };
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(masterclass.length / masterclassesPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
     return (
         <>
             <div className="masterclasses">
-                <div className={"category-title"}>Nos Masterclass</div>
-                <div className={"title"}>Les meilleurs masterclass</div>
+                <div className={'category-title'}>Nos Masterclass</div>
+                <div className={'title'}>Les meilleurs masterclass</div>
                 <div className="filter">
-                    <PwButton title={"Categorie"} variant={"primary"} size={"medium"}></PwButton>
-                    <PwButton title={"instrument"} variant={"primary"} size={"medium"}></PwButton>
-                    <PwButton title={"durée"} variant={"primary"} size={"medium"}></PwButton>
-                    <PwButton title={"Niveau"} variant={"primary"} size={"medium"}></PwButton>
+                    <PwButton
+                        title={'Categorie'}
+                        variant={'primary'}
+                        size={'medium'}
+                        onClick={() => applyFilter('Categorie')}
+                    ></PwButton>
+                    <PwButton
+                        title={'Instrument'}
+                        variant={'primary'}
+                        size={'medium'}
+                        onClick={() => applyFilter('Instrument')}
+                    ></PwButton>
+                    {/* Add more buttons for different filters */}
                 </div>
-                <div className={"masterclass-container"}> {renderMasterclasses()}</div>
-
+                <div className={'masterclass-container'}>{renderMasterclasses()}</div>
+                <div className="pagination">
+                    {pageNumbers.map((number) => (
+                        <span key={number} onClick={() => paginate(number)}>
+              {number}
+            </span>
+                    ))}
+                </div>
             </div>
-            <Footer></Footer>
+            <Footer />
         </>
     );
 }
 
-export default Masterclasspage;
+export default MasterclassPage;
